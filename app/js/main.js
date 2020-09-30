0745b6b8960a25f1d563c52275f7a8527756340e
liza-ru <elizaveta.ct@yandex.ru>
2020-10-01 00:18:50 +0500

    added users icons

--- app/js/main.js
+++ app/js/main.js
@@ -8,11 +8,3 @@ var mixer = mixitup('.products__inner-box');
 $('.product__page-slider').slick();
 
 
-// $('.fade').slick({
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     fade: true,
-//     cssEase: 'linear'
-// });
-
