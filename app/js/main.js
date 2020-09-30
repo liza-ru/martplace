c467b57b3698d7dfa3ca163b00fbd7de022f2bcc
liza-ru <elizaveta.ct@yandex.ru>
2020-09-30 16:00:23 +0500

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -4,3 +4,15 @@ var mixer = mixitup('.products__inner-box');
 
 
 });
+
+$('.product__page-slider').slick();
+
+
+// $('.fade').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 500,
+//     fade: true,
+//     cssEase: 'linear'
+// });
+
